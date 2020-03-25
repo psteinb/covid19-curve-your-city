5fb5281a279e30aea616cc30ffc4e7f9319309a4
Peter Steinbach <p.steinbach@hzdr.de>
2020-03-25 17:12:08 +0100

    simple tool to download RKI data based on https://github.com/nevrome/covid19germany

---
+++ data/RKI/download_data.R
@@ -0,0 +1,31 @@
+#from https://github.com/nevrome/covid19germany
+library(covid19germany)
+library(optparse)
+library(readr)
+library(dplyr)
+
+## DEFINING COMMAND LINE INTERFACE
+parser <- OptionParser(## description='process COVID19 data'
+)
+option_list <- list(
+  make_option(c('-o','--output'),
+              default='RKI_COVID.csv',
+              help='output file RKI data [default %default]')#,
+  ## make_option(c('-T','--titleextra'),
+  ##             default='',
+  ##             help='add this to the title [default %default]'),
+  ## make_option(c('-L','--logscale'),
+  ##             action="store_true",
+  ##             default=FALSE,
+  ##             help='include a logscale plot in a second column [default %default]')
+)
+opts = parse_args(OptionParser(option_list=option_list))
+
+if (is.null(opts$output)){
+  print_help(parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+
+df = covid19germany::get_RKI_timeseries()
+glimpse(df)
+write.csv(df,opts$output)
