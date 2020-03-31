365e4cb143f1f0d0ef2dd3f9f8e407d50cade486
vv01f <vv01f@users.noreply.github.com>
2020-03-31 18:59:07 +0200

    separate config from script

--- dresden-json2csv.sh
+++ dresden-json2csv.sh
@@ -15,55 +15,101 @@ assert_tools () {
 dependencies="printf cat cut rev test date sleep curl jq"
 assert_tools $dependencies
 
+while test "$#" -gt "0"; do
+	case "$1" in
+	*.json)
+		jsonfn="$1"
+		;;
+	*.config)
+		configfn="$1"
+		;;
+	*.csv)
+		csvfn="$1"
+		;;
+	noquery)
+		noquery="true"
+		;;
+	*)
+		echo $1
+		;;
+	esac
+	shift
+done
+
+# configuration file expecting e.g.
+#~ result="data/de_dresden_www.csv"
+#~ useragent="Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0"
+#~ query1="https://services.arcgis.com/ORpvigFPJUhb8RDF/arcgis/rest/services/corona_DD_3/FeatureServer/0/query?f=json&where=Anzeige_Indikator%3D%27x%27&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=50&cacheHint=true"
+#~ query2="https://services.arcgis.com/ORpvigFPJUhb8RDF/arcgis/rest/services/corona_DD_3/FeatureServer/0/query?f=json&where=Fallzahl%20IS%20NOT%20NULL&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=2000&cacheHint=true"
+#~ #timestamp='+%Y-%m-%dT%H:%M:%S'
+#~ csvheader="city,date,tod_hhmm,diagnosed,deceased,recovered,hospitalized"
+#~ defaultentryplace="Dresden"
+#~ defaultentrytod="12:00"
+#~ jsonquery="'.features[] .attributes | { Datum, Fallzahl, Sterbefall, Genesungsfall, Hospitalisierung }'"
+
+test -z "$configfn" && { configfn="data.config"; }
+
 # for calling existing file, fallback test case
-test -z $1 && fn="data/de_dresden_www.json" || fn="$1"
+test -z "$jsonfn" && jsonfn="data/de_dresden_www.json"
 # could test for existing file
 
-# disable query new file with cli option
-test "$2" = "noquery" || {
-
 # user agent
-a="Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0"
-# 1st request
-url1="https://services.arcgis.com/ORpvigFPJUhb8RDF/arcgis/rest/services/corona_DD_3/FeatureServer/0/query?f=json&where=Anzeige_Indikator%3D%27x%27&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=50&cacheHint=true"
-# 2nd request, needs 1st request, otherwise satus code 400
-url2="https://services.arcgis.com/ORpvigFPJUhb8RDF/arcgis/rest/services/corona_DD_3/FeatureServer/0/query?f=json&where=Fallzahl%20IS%20NOT%20NULL&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=2000&cacheHint=true"
+a=$(cat "$configfn" | grep -i "^useragent="|head -1|cut -d= -f2-|cut -d'"' -f2)
+test -z "$a" && a="Mozilla/5.0 (X11; Linux x86_64; rv:74.0) Gecko/20100101 Firefox/74.0"
 
-# alternative tool: wget
+# 1st request
+url1=$(cat "$configfn" | grep -i "^query1="|head -1|cut -d= -f2-|cut -d'"' -f2)
+test -z "$url1" && url1="https://services.arcgis.com/ORpvigFPJUhb8RDF/arcgis/rest/services/corona_DD_3/FeatureServer/0/query?f=json&where=Anzeige_Indikator%3D%27x%27&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=50&cacheHint=true"
+# 2nd request, needs 1st request, otherwise satus code 400
+url2=$(cat "$configfn" | grep -i "^query2="|head -1|cut -d= -f2-|cut -d'"' -f2)
+test -z "$url2" && url2="https://services.arcgis.com/ORpvigFPJUhb8RDF/arcgis/rest/services/corona_DD_3/FeatureServer/0/query?f=json&where=Fallzahl%20IS%20NOT%20NULL&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=2000&cacheHint=true"
+
+test -z "$csvheader" && { csvheader=$(cat "$configfn" | grep -i "^csvheader="|head -1|cut -d= -f2-|cut -d'"' -f2); }
+
+test -z "$csvfn" && { csvfn=$(cat "$configfn" | grep -i "^csvfn="|head -1|cut -d= -f2-|cut -d'"' -f2); }
+test -z "$csvfn" && { csvfn=$(printf "$jsonfn"|rev|cut -d. -f2-|rev)".csv"; }
+test -z "$jsonfn" && { jsonfn=$(cat "$configfn" | grep -i "^jsonfn="|head -1|cut -d= -f2-|cut -d'"' -f2); }
+test -z "$jsonfn" && { jsonfn=$(printf "$csvfn"|rev|cut -d. -f2-|rev)".json"; }
+test -z "$jsonfn" && { csvfn="data/de_dresden_www.json"; }
+test -z "$csvfn" && { jsonfn="data/de_dresden_www.csv"; }
+
+test -z "$ts" && { ts=$(cat "$configfn" | grep -i "^timestamp="|head -1|cut -d= -f2-|cut -d'"' -f2); }
+
+# disable query new file with cli option
+test "$noquery" = "true" && { printf "ommiting web requets."; } || {
+
+# alternative tool would be e.g. wget
 # here call is silent, without checks on certificates and with custom user agent (browser name/version etc.)
 curl -s -k -A "$a" "$url1" 2>&1 1>/dev/null
 # wait a moment to not trigger some protection on server side
 sleep 1 # although this doesnt seem to be necessarry at all
 # write actual content to file $fn
-curl -s -o "$fn" -k -A "$a" "$url2"
+curl -s -o "$jsonfn" -k -A "$a" "$url2"
 
 }
 
 # information on file names, print to stderr, assert on existing file
-1>&2 printf "Reading from: %s\n" "$fn"
-test -f "$fn" || { 1>&2 printf "file does not exist.\n"; exit 1; }
+1>&2 printf "Reading from:\t%s\n" "$jsonfn"
+test -f "$jsonfn" || { 1>&2 printf "file does not exist.\n"; exit 1; }
 # output file with ISO timestamp in filename
-ts='+%Y-%m-%dT%H:%M:%S'
-tf=$(printf "$fn"|rev|cut -d. -f2-|rev)".csv"
-# may or may not use timestamp as prefix
-#tf=$(date "$ts")"_$tf"
-1>&2 printf "Writing to: $tf\n"
+# or no timestamp as prefix
+test -z "$ts" || { path=$(printf "$csvfn"|rev|cut -d"/" -f2-|rev); csvfn=$(printf "$csvfn"|rev|cut -d"/" -f1|rev); csvfn="${path}/"$(date "$ts")"_$csvfn"; }
+1>&2 printf "Writing to:\t$csvfn\n"
 
 # check for file extension
-test "$(printf "$fn\n"|rev|cut -d. -f1|rev)" = "json" && {
+test "$(printf "$jsonfn\n"|rev|cut -d. -f1|rev)" = "json" && {
 
 # head line as found in csv of the repo
-1>"$tf" printf "city,date,tod_hhmm,diagnosed,deceased,recovered,hospitalized"
-# csv header as used in R script
-#~ 1>"$tf" printf "city,date,tod_hhmm,diagnosed"
+1>"$csvfn" printf "$csvheader\n"
 
 # following values are assumed constant
 # standard values for place and time
-p="Dresden"
-c="12:00" # time of date, format H:M
+p=$(cat "$configfn" | grep -i "defaultentryplace="|head -1|cut -d= -f2-|cut -d'"' -f2)
+# time of date, format H:M
+c=$(cat "$configfn" | grep -i "defaultentrytod="|head -1|cut -d= -f2-|cut -d'"' -f2)
 
 # process lines (each day) of the files, filter json with `jq`
-for i in $(cat $fn | jq -c '.features[] .attributes | { Datum, Fallzahl, Sterbefall, Genesungsfall, Hospitalisierung }');
+for i in $(cat $jsonfn | jq -c '.features[] .attributes | { Datum, Fallzahl, Sterbefall, Genesungsfall, Hospitalisierung }');
 do
 	# numbers, may need fix in case NAN
 	# n: number of cases
@@ -88,9 +134,9 @@ do
 
 	# output for csv
 	# including more numbers
-	1>>"$tf" printf "%s,%04d-%02d-%02d,%s,%s,%s,%s,%s\n" "$p" "$y" "$m" "$d" "$c" "$n" "$b" "$r" "$h"
+	1>>"$csvfn" printf "%s,%04d-%02d-%02d,%s,%s,%s,%s,%s\n" "$p" "$y" "$m" "$d" "$c" "$n" "$b" "$r" "$h"
 	# csv as used by R script, number infected only
-	#~ 1>>"$tf" printf "%s,%04d-%02d-%02d,%s,%s\n" "$p" "$y" "$m" "$d" "$c" "$n"
+	#~ 1>>"$csvfn" printf "%s,%04d-%02d-%02d,%s,%s\n" "$p" "$y" "$m" "$d" "$c" "$n"
 
 done
 	
